9f8de183c142337b720cc0632e9c7cd6b133ea4f
Evan Bonsignori <evanabonsignori@gmail.com>
2023-06-27 17:10:53 -0700

    add more actions

--- src/lib/determine-action.ts
+++ src/lib/determine-action.ts
@@ -6,13 +6,17 @@ export default function determineAction(
   notification: Endpoints["GET /notifications"]["response"]["data"][0]
 ): string | [string, string] {
   if (notification.reason === REASONS.MENTION) {
-    return `Mentioned in ${notification.subject.title}`;
+    return [`Mentioned in ${notification.subject.title}`, notification.subject.url];
   }
 
   if (notification.reason === REASONS.SUBSCRIBED) {
     if (notification.subject.latest_comment_url) {
-      return `New comment on ${notification.subject.title}`;
+      return [`New comment on ${notification.subject.title}`, notification.subject.latest_comment_url];
     }
+  }
+
+  if (notification.reason === REASONS.REVIEW_REQUESTED) { 
+    return [`Review requested on ${notification.subject.title}`, notification.subject.url];
   }
 
   return notification.subject.title
