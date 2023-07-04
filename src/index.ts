75be4ed01dbf666b5ecccbbbfc6cbb21bb87d537
Evan Bonsignori <evanabonsignori@gmail.com>
2023-07-04 16:49:52 -0700

    fix artifact import

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { throttling } from "@octokit/plugin-throttling";
 import { Endpoints } from "@octokit/types";
 import { WebClient } from "@slack/web-api";
 import Webex from "webex";
-import artifact from "@actions/artifact";
+import { create, create as createArtifact } from "@actions/artifact";
 import fs from "fs";
 
 import getInputs from "./lib/get-inputs";
@@ -168,7 +168,7 @@ async function run(
         "notifications.json",
         JSON.stringify(notifications, null, 2)
       );
-      const artifactClient = artifact.create();
+      const artifactClient = createArtifact();
       await artifactClient.uploadArtifact(
         "notifications",
         ["notifications.json"],
